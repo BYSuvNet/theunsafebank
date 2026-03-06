0d4cafac90c986e8f7c601d224141889df85fd6b
Jonas Persson <jonas.persson111@gmail.com>
2026-03-06 12:54:37 +0100

    samesite set to strict

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddSession(options =>
     options.IdleTimeout = TimeSpan.FromSeconds(10);
     options.Cookie.HttpOnly = true;
     options.Cookie.IsEssential = true;
+    options.Cookie.SameSite = SameSiteMode.Strict;
 });
 
 var app = builder.Build();

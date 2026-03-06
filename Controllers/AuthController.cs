f8acde62b9072497e97f0ad9ebb8bf902d9be0e3
Micaela <micaela95live.se>
2026-03-06 13:25:31 +0100

    Fix password validation error messages for clarity and consistency

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -53,32 +53,27 @@ public class AuthController : Controller
             return View();
         }
 
-        // Password längd
         if (password.Length < 8)
         {
-            ViewBag.Error = "Lösenordet måste vara 8 tecken lång";
+            ViewBag.Error = "Lösenordet måste vara 8 tecken långt";
             return View();
         }
 
-        // Password Specialtecken och siffror
         if (!password.Any(char.IsNumber) && !password.Any(char.IsSymbol))
         {
             ViewBag.Error = "Lösenordet måste innehålla siffror och specialtecken";
             return View();
         }
 
-        // Password Stor bokstav 
-
         if (!password.Any(char.IsUpper))
         {
-            ViewBag.Error = "Lösenordet måste innehålla Storbokstav";
+            ViewBag.Error = "Lösenordet måste innehålla storbokstav";
             return View();
         }
 
-        // Password samma som användarnamn
         if (password == username)
         {
-            ViewBag.Error = "Lösenorder får inte vara samma som användarnamn";
+            ViewBag.Error = "Lösenordet får inte vara samma som användarnamn";
             return View();
         }
 
